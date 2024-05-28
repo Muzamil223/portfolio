ea490c4f3c6519eb103d9ea356a10e53c49b2271
Muzamil223 <muzamil987614@gmail.com>
2024-05-29 03:37:07 +0500

    first commit

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,14 +9,14 @@ const About = () => {
       <h1 className="my-20 text-center text-4xl">
         About<span className="text-neutral-500"> Me</span>
       </h1>
-      <div className="flex flex-wrap">
+      <div className="flex flex-wrap ">
         <motion.div
           whileInView={{ opacity: 1, x: 0 }}
           initial={{ opacity: 0, x: -100 }}
           transition={{ duration: 0.5 }}
           className="w-full lg:w-1/2 lg:p-8"
         >
-          <div className="flex items-center justify-center ">
+          <div className="flex items-center hidden lg:block justify-center ">
             <img src={front} alt="about" className="w-[80%] rounded-2xl" />
           </div>
         </motion.div>
